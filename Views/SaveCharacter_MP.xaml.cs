526cae9c1e6f89de587924095fb19d6a4d230c19
Mundrack <mateopuga75@gmail.com>
2025-01-20 10:00:17 -0500

    Creamos Views donde tenemsos SaveCharacter para mostrar algunas caracteristicas guardadas y en wowpage mmostramos la interfas para la interaccion con la appi para la busqueda. Ademas modificamos el app shell para la facil interaccion con wow y personajes guardados.

---
+++ Views/SaveCharacter_MP.xaml.cs
@@ -0,0 +1,9 @@
+namespace PracticaWoW_MP.Views;
+
+public partial class SaveCharacter_MP : ContentPage
+{
+	public SaveCharacter_MP()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Views/WowPage_MP.xaml.cs
@@ -0,0 +1,9 @@
+namespace PracticaWoW_MP.Views;
+
+public partial class WowPage_MP : ContentPage
+{
+	public WowPage_MP()
+	{
+		InitializeComponent();
+	}
+}
